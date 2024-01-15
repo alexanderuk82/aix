9cbeb5d77a084ec889f78f3760062fd5e030a8c8
alexDev <info@alexandersstudio.uk>
2024-01-15 03:49:53 +0000

    Initial commit from Create Next App

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Create Next App',
+  description: 'AIX is a project for UX researches and designers to create and share design systems.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
